988b23658d833ba80aa419a53a3ac26c3cc54001
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2016-07-13 16:12:05 +0100

    Refactor BinarySearch to work as an object

--- lib/binary_search.rb
+++ lib/binary_search.rb
@@ -1,21 +1,25 @@
 # BinarySearch examples
 class BinarySearch
+  def initialize(array)
+    @array = array
+    @size = array.size
+  end
+
   # Find the position of a number in a sorted array
   # This is known as binary search and essentially boils down to dissecting
   # the array into smaller and smaller chunks until you get a match, as opposed
   # to searching the whole array. Have seen this specified as O(log n) vs O(n)
   # (O(n) being bad)
-  def self.where_in_array(array, number)
-    size = array.size
-    high = size - 1
+  def where_in_array(number)
+    high = @size - 1
     low = 0
 
     while low <= high
       mid = (low + high) / 2
       # puts "low #{low}, high #{high}, mid#{mid}"
-      if number == array[mid]
+      if number == @array[mid]
         return mid
-      elsif number < array[mid]
+      elsif number < @array[mid]
         high = mid - 1
       else
         low = mid + 1
@@ -30,19 +34,18 @@ class BinarySearch
   # the array into smaller and smaller chunks until you get a match, as opposed
   # to searching the whole array. Have seen this specified as O(log n) vs O(n)
   # (O(n) being bad)
-  def self.first_in_array(array, number)
-    size = array.size
-    high = size - 1
+  def first_in_array(number)
+    high = @size - 1
     low = 0
     result = -1
 
     while low <= high
       mid = (low + high) / 2
       # puts "low #{low}, high #{high}, mid#{mid}"
-      if number == array[mid]
+      if number == @array[mid]
         result = mid
         high = mid - 1
-      elsif number < array[mid]
+      elsif number < @array[mid]
         high = mid - 1
       else
         low = mid + 1
@@ -57,19 +60,18 @@ class BinarySearch
   # the array into smaller and smaller chunks until you get a match, as opposed
   # to searching the whole array. Have seen this specified as O(log n) vs O(n)
   # (O(n) being bad)
-  def self.last_in_array(array, number)
-    size = array.size
-    high = size - 1
+  def last_in_array(number)
+    high = @size - 1
     low = 0
     result = -1
 
     while low <= high
       mid = (low + high) / 2
       # puts "low #{low}, high #{high}, mid#{mid}"
-      if number == array[mid]
+      if number == @array[mid]
         result = mid
         low = mid + 1
-      elsif number < array[mid]
+      elsif number < @array[mid]
         high = mid - 1
       else
         low = mid + 1
@@ -84,11 +86,11 @@ class BinarySearch
   # the array into smaller and smaller chunks until you get a match, as opposed
   # to searching the whole array. Have seen this specified as O(log n) vs O(n)
   # (O(n) being bad)
-  def self.count_in_array(array, number)
-    first = first_in_array(array, number)
+  def count_in_array(number)
+    first = first_in_array(number)
     return 0 if first == -1
 
-    last = last_in_array(array, number)
+    last = last_in_array(number)
     (last - first + 1)
   end
 end
